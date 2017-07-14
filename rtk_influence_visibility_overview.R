10968c133d176280e9db6a5301dcfb60bf56a697
lankrist <lankrist@umich.edu>
2017-07-14 16:01:41 -0400

    legend colors

--- rtk_influence_visibility_overview.R
+++ rtk_influence_visibility_overview.R
@@ -29,15 +29,19 @@ class(rtk_[1,3])
 rtk_[,2:8]=apply(rtk_[,2:8], MARGIN = 2, as.numeric)
 totals = apply(rtk_[,2:8], MARGIN = 2, sum)
 totalB=sum(totals[c(1,4,6)])
+totalb=paste('$',formatC(totalB, big.mark=',', digits = 2, format = "fg")) #format
+
 pie(totals[c(1,4,6)], labels = c("RTK", "commodities (non-RTK)", "TA"), 
-    main = "Investment Distribution")
+    main = paste("Investment Distribution 2016 ( ",totalb, ")"))
+totals[c(1,4,6)]/totalB
+#rough idea on how money is divided
 
 #Influence levels (numeric)
+#(rtk_$`2016_rtk_gf`+rtk_$`2016_rtk_usaid`) 
+rtk_$ghusaid = ifelse((rtk_[,2]+rtk_[,3]) != 0, rtk_[,2]/(rtk_[,2]+rtk_[,3]),0)
 
-rtk_$influence = 
-  
-rtk_$influence  <- factor(rtk_$influence , levels=levels, 
-                          ordered = is.ordered(levels_I)) 
+rtk_$influence = log(rtk_$`2016_rtk_usaid`*rtk_$ghusaid +rtk_$`2016_TA` + rtk_$`2016_non-rtk_com`+1)
+plot(rtk_$influence)
 
 #visiblity levels
 level = c('Low','Low-medium', 'Medium','High' ) #visibility
@@ -50,33 +54,42 @@ rtk_[rtk_$G4== 'yes'& rtk_$G5 =='yes'& rtk_$G6 =='yes','visibility'] = level[4]
 rtk_$visibility  <- factor(rtk_$visibility  , levels=level,
                            ordered = is.ordered(levels_V)) 
 
+#investment
+rtk_$investment = rtk_$`2016_rtk_usaid`+rtk_$`2016_non-rtk_com` + rtk_$`2016_TA`
 
-#Order infleunce and visibility by increasing; investment by decreasing
-rtk_= rtk_[order(rtk_$influence,rtk_$visibility, -rtk_$investment),]
 
+#SIZE
+size <- sqrt(rtk_$investment/pi)
+N = nrow(rtk_)
+#COLOR
+rtk_ = rtk_[order(rtk_$visibility),]
+rtk_$op = unlist(sapply(table(rtk_$visibility), heat.colors))
+# leg_rtk = rtk_$country; leg_op = rtk_$op #legend colors
 
+#ORDER infleunce and visibility by increasing; investment by decreasing
+rtk_= rtk_[order(-rtk_$influence,rtk_$visibility, -rtk_$investment),]
 
 # dev.new(width=6, height=5) #customize plot size
 # plot(table(rtk_$influence, rtk_$visibility, dnn = c('Influence', 'Visibility')))
 
 dev.off()
-dev.new(width=6, height=5) #customize plot size
-layout(cbind(1,2), widths = c(5,1)); layout.show(2)
-size <- sqrt(rtk_$investment/pi)
-N = nrow(rtk_)
+dev.new(width=10, height=5) #customize plot size
+layout(cbind(1,2), widths = c(3,1)); layout.show(2)
 
-op =c(heat.colors(N))
+#GRAPH
+par(mar = c(4, 4, 2, 2), oma = c(0, 0, 0, 0))
 with(rtk_, {
-  par(mai=c(1, 1, 0.3, 2.4), xpd=TRUE)
-  palette(op)
+  palette(rtk_$op)
   symbols(rtk_$visibility, rtk_$influence, circles = size/24, 
           bg = 1:N, #main = "Influence and Visibility of USAID Investment by Country",
           ylab = "Influence", xlab = "Visibility")
 })
 plot.new()
-legend("center", inset=c(-0.5),legend=rtk_$country, y= NULL, ,pch = 15, 
-       col = palette(op), bty="n")
+par(mar = c(0,0, 0, 0))
+legend("center",legend=rtk_$country, y= NULL, ,pch = 15, 
+       col = palette(rtk_$op), bty="n")
 #legend(bty="n") removes box around legend 
+#why legend "right" doesn't give right alignment
 
 #how to overlay country names
 #how to show legends with break down of type
