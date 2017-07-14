9666555a66e4322f9a8a2f3573aaa06d58f7f805
lankrist <lankrist@umich.edu>
2017-07-14 12:18:47 -0400

    formatting

--- rtk_influence_visibility_overview.R
+++ rtk_influence_visibility_overview.R
@@ -1,6 +1,8 @@
 #influence and visibility visualization
 #as-hoc report for OGAC slides 2017/07/07
+library(graphics) #piechart()
 #setwd('USAID_Internship2017/dataset/')
+
 
 rtk=read.csv(file = 'Global RTK Investment Profile_edit.csv', header = T, stringsAsFactors = F)
 
@@ -19,15 +21,21 @@ rtk_$country[10] = "Cote d'Ivoire" #fix country name
 
 View(rtk_)
 
-#Influence levels
-levels = c('Low', 'Medium', 'Medium-high', 'High')
-levels_I = 1:4
+#Calculations and converstions
+class(rtk_[1,3])
+#convert money to numbers (data loaded in as string)
+# rtk_[,c(2:8)] = as.numeric(gsub('\\$|,','',rtk_$total_investment))
+# apply(rtk_[,c(2:8)], MARGIN = 2, function(x) as.numeric(gsub("\\$|,","", rtk_[,c(2:8)])))
+rtk_[,2:8]=apply(rtk_[,2:8], MARGIN = 2, as.numeric)
+totals = apply(rtk_[,2:8], MARGIN = 2, sum)
+totalB=sum(totals[c(1,4,6)])
+pie(totals[c(1,4,6)], labels = c("RTK", "commodities (non-RTK)", "TA"), 
+    main = "Investment Distribution")
 
-rtk_$influence = levels[1]
-rtk_[rtk_$G1 == 'x','influence'] = levels[3] #do not change sequence
-rtk_[rtk_$G1 == 'x' & rtk_$G1a == 'x','influence'] = levels[4]
-rtk_[rtk_$G2 == 'x','influence'] = levels[2]
+#Influence levels (numeric)
 
+rtk_$influence = 
+  
 rtk_$influence  <- factor(rtk_$influence , levels=levels, 
                           ordered = is.ordered(levels_I)) 
 
@@ -43,30 +51,31 @@ rtk_$visibility  <- factor(rtk_$visibility  , levels=level,
                            ordered = is.ordered(levels_V)) 
 
 
-#convert money to numbers (data loaded in as string)
-rtk_$investment = as.numeric(gsub('\\$|,','',rtk_$total_investment))
-#order infleunce and visibility by increasing; investment by decreasing
+#Order infleunce and visibility by increasing; investment by decreasing
 rtk_= rtk_[order(rtk_$influence,rtk_$visibility, -rtk_$investment),]
+
+
 
 # dev.new(width=6, height=5) #customize plot size
 # plot(table(rtk_$influence, rtk_$visibility, dnn = c('Influence', 'Visibility')))
 
 dev.off()
 dev.new(width=6, height=5) #customize plot size
-
+layout(cbind(1,2), widths = c(5,1)); layout.show(2)
 size <- sqrt(rtk_$investment/pi)
 N = nrow(rtk_)
+
+op =c(heat.colors(N))
 with(rtk_, {
   par(mai=c(1, 1, 0.3, 2.4), xpd=TRUE)
-  op =c(heat.colors(N))
   palette(op)
   symbols(rtk_$visibility, rtk_$influence, circles = size/24, 
           bg = 1:N, #main = "Influence and Visibility of USAID Investment by Country",
           ylab = "Influence", xlab = "Visibility")
-  
-  legend("right", inset=c(-0.5),legend=rtk_$country, y= NULL, ,pch = 15, 
-         col = palette(op), bty="n")
 })
+plot.new()
+legend("center", inset=c(-0.5),legend=rtk_$country, y= NULL, ,pch = 15, 
+       col = palette(op), bty="n")
 #legend(bty="n") removes box around legend 
 
 #how to overlay country names
