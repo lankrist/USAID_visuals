8abd319bf446dd8e535a11179bdf79cf41228b58
lankrist <lankrist@umich.edu>
2017-07-17 13:34:21 -0400

    pie chart

--- rtk_influence_visibility_overview.R
+++ rtk_influence_visibility_overview.R
@@ -1,8 +1,11 @@
 #influence and visibility visualization
 #as-hoc report for OGAC slides 2017/07/07
-library(graphics) #piechart()
+library(graphics) #piechar  t()
+library(ggplot2)
+library(scales) #format labels in percent
+library(plyr) #mutate
+library(dplyr) #%>%
 #setwd('USAID_Internship2017/dataset/')
-
 
 rtk=read.csv(file = 'Global RTK Investment Profile_edit.csv', header = T, stringsAsFactors = F)
 
@@ -15,14 +18,14 @@ colnames(rtk_) = c("country", "2016_rtk_usaid", "2016_rtk_gf", "2017_rtk_budget"
                    "2017_TA_budget", "G4", "G5", "G6")
 rtk_ = rtk_[-1,] #exlude row 1, go back to excel formatting
 #c("Angola", "Caribbean", "Central America", "Burma", "Guyana", "Ukraine", "Namibia")
-rtk_ =rtk_[-c(32),] #exclude empty rows in the end
+rtk_ =rtk_[-c(32:33),] #exclude empty rows in the end
 rtk_=rtk_[-c(1,3,7,8,14,20,28),] #exlude countries listed above
 rtk_$country[10] = "Cote d'Ivoire" #fix country name
 
 View(rtk_)
 
-#Calculations and converstions
-class(rtk_[1,3])
+#Calculations and conversions
+
 #convert money to numbers (data loaded in as string)
 # rtk_[,c(2:8)] = as.numeric(gsub('\\$|,','',rtk_$total_investment))
 # apply(rtk_[,c(2:8)], MARGIN = 2, function(x) as.numeric(gsub("\\$|,","", rtk_[,c(2:8)])))
@@ -35,6 +38,39 @@ pie(totals[c(1,4,6)], labels = c("RTK", "commodities (non-RTK)", "TA"),
     main = paste("Investment Distribution 2016 ( ",totalb, ")"))
 totals[c(1,4,6)]/totalB
 #rough idea on how money is divided
+
+#GGPLOT2 method
+dfp = data.frame(
+  group = c("RTK", "Commodities (non-RTK)", "TA"),
+  value = c(totals[c(1,4,6)])
+)
+dfp =dfp[order(dfp$value),] #has to be ordered to make labels make sense
+
+bp = ggplot(dfp, aes(x ="", y = value, fill = group))+
+  geom_bar(width=1, stat = "identity")
+bp #barplot
+
+pie = bp + coord_polar(theta = "y", start = 0)
+pie
+
+#create blank theme
+blank_theme = theme_minimal() + 
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.ticks = element_blank(),
+    plot.title = element_text(size = 14, face = "bold")
+  )
+
+#label positioning
+dfp = mutate(dfp, pos = cumsum(value) - value/3)
+
+pie + scale_fill_brewer("Expense") + blank_theme +
+  theme(axis.text.x = element_blank())+
+  geom_text(aes(x = 1,y = dfp$pos,label = percent(value/totalB)),size = 5)
+#  facet_grid(facets =.~group,labeller = label_value)
 
 #Influence levels (numeric)
 #(rtk_$`2016_rtk_gf`+rtk_$`2016_rtk_usaid`) 
@@ -67,7 +103,7 @@ rtk_$op = unlist(sapply(table(rtk_$visibility), heat.colors))
 # leg_rtk = rtk_$country; leg_op = rtk_$op #legend colors
 
 #ORDER infleunce and visibility by increasing; investment by decreasing
-rtk_= rtk_[order(-rtk_$influence,rtk_$visibility, -rtk_$investment),]
+rtk_= rtk_[order(-rtk_$influence,rtk_$visibility, rtk_$investment),]
 
 # dev.new(width=6, height=5) #customize plot size
 # plot(table(rtk_$influence, rtk_$visibility, dnn = c('Influence', 'Visibility')))
@@ -80,7 +116,7 @@ layout(cbind(1,2), widths = c(3,1)); layout.show(2)
 par(mar = c(4, 4, 2, 2), oma = c(0, 0, 0, 0))
 with(rtk_, {
   palette(rtk_$op)
-  symbols(rtk_$visibility, rtk_$influence, circles = size/24, 
+  symbols(rtk_$visibility, rtk_$influence, circles = size/16, 
           bg = 1:N, #main = "Influence and Visibility of USAID Investment by Country",
           ylab = "Influence", xlab = "Visibility")
 })
